5836c193f777ea0be19847f3302b62484da9f6eb
Alexander Stromberger <alexander.stromberger@st.fyi>
2023-07-19 23:52:44 +0200

    changed layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'st dot fyi (Alexander Stromberger)';
+export const SITE_TITLE = 'Alexander Stromberger';
 export const SITE_DESCRIPTION = 'A site about random things that might or might not be interesting!';
