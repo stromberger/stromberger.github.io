313882194ed702b01c01068cf3c12771bf777417
Alexander Stromberger <alexander.stromberger@st.fyi>
2024-05-02 13:03:40 +0200

    new design

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Alexander Stromberger';
-export const SITE_DESCRIPTION = 'A site about random things that might or might not be interesting!';
